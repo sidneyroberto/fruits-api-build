3eb4ff2c27f957d9323160c553971237998d4247
Sidney Sousa <sidney.sousa@ifms.edu.br>
2022-11-17 11:23:44 -0400

    commit inicial

---
+++ api/index.js
@@ -0,0 +1,36 @@
+const app = require('express')()
+const {
+  v4
+} = require('uuid')
+const axios = require('axios')
+
+
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`)
+});
+
+app.get('/api/fruits/:name', async (req, res) => {
+  const {
+    name
+  } = req.params
+  try {
+    const response = await axios.get(
+      `https://fruityvice.com/api/fruit/${name.toLowerCase().trim()}`
+    )
+
+    if (response.status == 200) {
+      return res.json(response.data)
+    }
+  } catch (error) {
+    console.log(error)
+  }
+
+  return res.status(404).json({
+    message: 'Fruit not found'
+  })
+});
+
+module.exports = app
