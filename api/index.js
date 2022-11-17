bd00396be58100baf019a391bace10b65407dd29
Sidney Sousa <sidney.sousa@ifms.edu.br>
2022-11-17 11:26:11 -0400

    ajustando raiz

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ app.get('/api', (req, res) => {
   const path = `/api/item/${v4()}`
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`)
+  res.end(`Fruits API. A wrapper for Fruityvice.<br>Endpoint: /fruits/:name`)
 });
 
 app.get('/api/fruits/:name', async (req, res) => {
